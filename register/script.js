834ea888d73ebddffb64f12547f0762a77d20ae9
Hattori <crafter.alloy@gmail.com>
2024-05-20 19:59:28 +0900

    登録フォームの作成

---
+++ register/script.js
@@ -0,0 +1,26 @@
+status_elements = document.getElementsByName("student-or-general");
+status_radio = document.getElementById("status-box");
+student_detail = document.getElementById("student-detail");
+
+status_radio.addEventListener("click",() => {changed_status()})
+
+function get_status_value() {
+  
+}
+
+function changed_status() {
+  person_status = ""
+  status_elements.forEach(e => {
+    if (e.checked) {
+      person_status = e.value;
+    }
+  });
+  if (person_status == "student") {
+    student_detail.style.display = "block";
+  } else {
+    student_detail.style.display = "none";
+  }
+}
+
+
+
